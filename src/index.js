240a5aa5fed8952d8e1be6e78383520b3ad547b8
alexeevaDaria <mari.marmeladkina@mail.ru>
2018-09-21 23:38:14 +0300

    Complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var a,b,c,D=0;
+
+  var arguments=equation.replace(/\s/g, "" );
+  arguments= arguments.match(/\-?[0-9]+/gi);
+  a=Number(arguments[0]);
+  b=Number(arguments[2]);
+  c=Number(arguments[3]);
+
+  D=Math.pow(b,2)-4*a*c;
+
+  var x1,x2;
+  x1=Math.round((-b-Math.sqrt(D))/(2*a));
+  x2=Math.round((-b+Math.sqrt(D))/(2*a));
+
+  return x1 > x2 ? [x2, x1] : [x1, x2];
 }
